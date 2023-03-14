d86bb883d468da92d34c43df04a3177fe8242bd7
Albert <aphan@mines.edu>
2023-03-13 21:48:51 -0600

    reset button now sized

--- app/src/main/java/com/collinbugash/swipeify/presentation/settings/ResetButton.kt
+++ app/src/main/java/com/collinbugash/swipeify/presentation/settings/ResetButton.kt
@@ -1,6 +1,7 @@
 package com.collinbugash.swipeify.presentation.settings
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,13 +9,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.collinbugash.swipeify.R
 
 @Composable
 fun ResetButton(onClick: () -> Unit){
     Button(
-        modifier = Modifier.fillMaxWidth(),
-        onClick = onClick
+        modifier = Modifier
+            .width(130.dp),
+        onClick = onClick,
     ) {
         Text(
             text = stringResource(id = R.string.reset),
